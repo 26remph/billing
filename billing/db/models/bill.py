cb09348822b5c38a5274c8c3fc128d49bfa942a4
Vadim Barsukov <v.bars@icloud.com>
2023-12-17 19:58:17 +0300

    - biling model clear

--- billing/db/models/bill.py
+++ billing/db/models/bill.py
@@ -1,7 +1,5 @@
-import asyncio
 import uuid
 from datetime import datetime
-from typing import Optional
 from uuid import UUID
 
 from sqlalchemy import func, DateTime, Float, String, ForeignKey, Text, Enum, select
@@ -35,11 +33,8 @@ class CartItem(DeclarativeBase, CommonFieldMixin):
 class ItemQuantity(CommonFieldMixin, DeclarativeBase):
     __tablename__ = "item_quantity"
 
-    # available: Optional[float] = None
     available: Mapped[float] = mapped_column(Float, nullable=True)
-    # count: float
     count: Mapped[float] = mapped_column(Float, nullable=False)
-    # label: Optional[str] = None
     label: Mapped[str] = mapped_column(String(255), nullable=True)
 
 
@@ -47,25 +42,13 @@ class Item(CommonFieldMixin, DeclarativeBase):
     __tablename__ = "item"
 
     productId: Mapped[UUID] = mapped_column(nullable=False, default=uuid.uuid4)
-    # productId: UUID
-    # quantity: ItemQuantity
     item_quantity_id: Mapped[UUID] = mapped_column(ForeignKey("item_quantity.id", ondelete="CASCADE"), nullable=False)
-    # item_quantity_id: UUID = Field(default=None, foreign_key='itemquantity.id')
-    # discountedUnitPrice: Optional[float] = None
     discountedUnitPrice: Mapped[float] = mapped_column(Float, nullable=True)
-    # finalPrice: Optional[float] = None
     finalPrice: Mapped[float] = mapped_column(Float, nullable=True)
-    # measurements: Measurements | None = None
-    # receipt: ItemReceipt | None = None
     subtotal: Mapped[float] = mapped_column(Float, nullable=True)
-    # subtotal: Optional[float] = None
     title: Mapped[str] = mapped_column(Text, nullable=True)
-    # title: Optional[str] = None
-    # total: Optional[float] = None
     total: Mapped[float] = mapped_column(Float, nullable=True)
-    # type: Optional[ItemType] = ItemType.UNSPECIFIED
     type: Mapped[str] = mapped_column(Enum(ItemType), default=ItemType.UNSPECIFIED)
-    # unitPrice: Optional[float] = None
     unitPrice: Mapped[float] = mapped_column(Float, nullable=True)
     carts: Mapped[list["Cart"]] = relationship(
         secondary="cart_item", back_populates="items", lazy="selectin"
@@ -78,17 +61,9 @@ class Cart(CommonFieldMixin, DeclarativeBase):
     __tablename__ = "cart"
 
     order: Mapped[list["Order"]] = relationship(back_populates="cart", lazy="selectin")
-    # items: list[CartItem]
-    # cartId: Optional[str] = None
     cartId: Mapped[str] = mapped_column(String(255), nullable=True)
-    # coupons: list[Coupon] | None = None
-    # discounts: list[Discount] | None = None
-    # externalId: Optional[str] = None
     externalId: Mapped[str] = mapped_column(String(255), nullable=True)
-    # measurements: Measurements | None = None
-    # total: CartTotal | None = None
     total: Mapped[float] = mapped_column(Float, nullable=True)
-    # total: CartTotal | None = None
     items: Mapped[list["Item"]] = relationship(
         secondary="cart_item", back_populates="carts", lazy="selectin"
     )
@@ -97,28 +72,15 @@ class Cart(CommonFieldMixin, DeclarativeBase):
 class Order(CommonFieldMixin, DeclarativeBase):
     __tablename__ = "order"
 
-    # cart: Cart
     cart: Mapped[list["Cart"]] = relationship(back_populates="order", lazy="selectin")
     cart_id: Mapped[UUID] = mapped_column(ForeignKey("cart.id", ondelete="CASCADE"), nullable=False)
-    # cart_id: UUID = Field(default=None, foreign_key='cart.id')
-    # currencyCode: CurrencyCode
     currencyCode: Mapped[str] = mapped_column(Enum(CurrencyCode), nullable=False, default=CurrencyCode.RUB)
-    # created: Optional[datetime] = None
     merchantId: Mapped[UUID] = mapped_column(default=uuid.uuid4, nullable=True)
-    # merchantId: Optional[UUID] = None
-    # metadata: str | None = None
-    # orderAmount: Optional[float] = None
     orderAmount: Mapped[float] = mapped_column(Float, nullable=True)
     orderId: Mapped[UUID] = mapped_column(nullable=True, default=uuid.uuid4)
-    # orderId: Optional[UUID] = None
-    # paymentMethod: PaymentMethod | None = None
-    # paymentStatus: Optional[PaymentStatus] = None
     paymentStatus: Mapped[str] = mapped_column(Enum(PaymentStatus), nullable=True)
-    # paymentUrl: Optional[str] = None
     paymentUrl: Mapped[str] = mapped_column(Text, nullable=True)
     reason: Mapped[str] = mapped_column(Text, nullable=True)
-    # shippingMethod: ShippingMethod | None = None
-    # updated: Optional[datetime] = None
 
 
 class Operation(CommonFieldMixin, DeclarativeBase):
@@ -126,20 +88,10 @@ class Operation(CommonFieldMixin, DeclarativeBase):
     __tablename__ = "operation"
 
     amount: Mapped[float] = mapped_column(Float, nullable=False)
-    # amount: float
     approvalCode: Mapped[str] = mapped_column(String(255), nullable=True)
-    # approvalCode: Optional[str] = None
-    # created: Optional[datetime] = None
     externalOperationId: Mapped[str] = mapped_column(String(255), nullable=True)
     operationId: Mapped[UUID] = mapped_column(nullable=False, default=uuid.uuid4)
-    # operationId: UUID
-    # operationType: OperationType
     operationType: Mapped[str] = mapped_column(Enum(OperationType))
-    # orderId: UUID = Field(default=None, foreign_key='order.id')
     orderId: Mapped[UUID] = mapped_column(ForeignKey("order.id", ondelete="CASCADE"), nullable=False)
-    # params: Dict[str, Any] | None = None
     reason: Mapped[str] = mapped_column(Text, nullable=True)
-    # reason: Optional[str] = None
     status: Mapped[str] = mapped_column(Enum(OperationStatus), default=OperationStatus.PENDING)
-    # status: Optional[OperationStatus] = OperationStatus.PENDING
-    # updated: Optional[str] = None
